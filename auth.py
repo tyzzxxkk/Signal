cc28a3aa8c52e90921e551169ed184fae0a1a595
hjzhhhj <jhj090120@gmail.com>
2025-05-14 01:00:11 +0900

    회원가입 로그인 예외처리 기능 추가 및 스타일 개선

--- auth.py
+++ auth.py
@@ -7,10 +7,6 @@ from forms import LoginForm, RegisterForm
 
 auth_bp = Blueprint('auth', __name__)
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
 @auth_bp.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
@@ -19,14 +15,17 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
-        if user and check_password_hash(user.password, form.password.data):
+        if not user:
+            flash('가입되지 않은 이메일입니다.', 'danger')
+        elif not check_password_hash(user.password, form.password.data):
+            flash('비밀번호가 일치하지 않습니다.', 'danger')
+        else:
             login_user(user)
             flash('로그인 성공!', 'success')
             return redirect(url_for('main.home'))
-        else:
-            flash('이메일 또는 비밀번호가 틀렸습니다.', 'danger')
 
     return render_template('login.html', form=form)
+
 
 @auth_bp.route('/register', methods=['GET', 'POST'])
 def register():
@@ -35,9 +34,11 @@ def register():
 
     form = RegisterForm()
     if form.validate_on_submit():
-        existing_user = User.query.filter_by(email=form.email.data).first()
-        if existing_user:
+        user = User.query.filter_by(email=form.email.data).first()
+        if user:
             flash('이미 존재하는 이메일입니다.', 'danger')
+        elif form.password.data != form.confirm.data:
+            flash('비밀번호가 일치하지 않습니다.', 'danger')
         else:
             hashed_pw = generate_password_hash(form.password.data)
             new_user = User(email=form.email.data, password=hashed_pw)
@@ -47,9 +48,3 @@ def register():
             return redirect(url_for('auth.login'))
 
     return render_template('register.html', form=form)
-
-@auth_bp.route('/logout')
-def logout():
-    logout_user()
-    flash('로그아웃 되었습니다.', 'info')
-    return redirect(url_for('auth.login'))

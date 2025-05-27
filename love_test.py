964f6bd2b5197d40ad1265423d84a401f53d9ab9
hjzhhhj <jhj090120@gmail.com>
2025-05-27 10:08:23 +0900

    lovetest theendgit add .git add .

--- love_test.py
+++ love_test.py
@@ -3,11 +3,11 @@ from utils import calculate_compatibility
 def love_result_message(name1, name2):
     score = calculate_compatibility(name1, name2)
     if score >= 90:
-        msg = "운명의 상대! 완벽한 궁합!"
+        msg = "헐... 우리 혹시... 결혼까지....?"
     elif score >= 70:
-        msg = "꽤 잘 맞는 편이에요!"
+        msg = "우리.... 연애 한 번 해볼까...?"
     elif score >= 50:
-        msg = "노력하면 잘 될 사이!"
+        msg = "우리 둘이 노력하면,,,, 뚜뚜루뚜뚜 가능!"
     else:
-        msg = "친구로 지내기에 딱 좋아요!"
+        msg = "띠로리"
     return score, msg

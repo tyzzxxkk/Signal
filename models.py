cc7386ee082c603d5b081c0f11ae6aa5a664743c
hjzhhhj <jhj090120@gmail.com>
2025-06-09 00:10:16 +0900

    migrations

--- models.py
+++ models.py
@@ -7,6 +7,21 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
     love_histories = db.relationship('LoveHistory', backref='user', lazy=True)
+    name = db.Column(db.String(100), nullable=True)
+
+class Letter(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    sender_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
+    receiver_email = db.Column(db.String(120), nullable=False) 
+    sender_name = db.Column(db.String(100), nullable=False) 
+    content = db.Column(db.Text, nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    is_anonymous = db.Column(db.Boolean, default=False)
+
+    sender = db.relationship('User', backref='sent_letters', foreign_keys=[sender_id]) 
+
+    def __repr__(self):
+        return f"<Letter from {self.sender_name} to {self.receiver_email}>"
 
 class LoveHistory(db.Model):
     id = db.Column(db.Integer, primary_key=True)

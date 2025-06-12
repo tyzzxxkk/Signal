f9fc6a88c810af0dc23b9c1fbfac00210262c76c
hjzhhhj <jhj090120@gmail.com>
2025-06-13 00:56:51 +0900

    [fix] : 편지 보내기 히스토리가 삭제가 안되는 오류 수정 / 히스토리가 많아지면 안보이는 오류 수정  /  히스토리, 편지함 레이아웃 수정

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from extensions import db
 from flask_login import UserMixin
-import datetime
+import datetime 
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -12,25 +12,25 @@ class User(db.Model, UserMixin):
 class Letter(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     sender_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
-    receiver_email = db.Column(db.String(120), nullable=False) 
-    sender_name = db.Column(db.String(100), nullable=False) 
+    receiver_email = db.Column(db.String(120), nullable=False)
+    sender_name = db.Column(db.String(100), nullable=False)
     content = db.Column(db.Text, nullable=False)
     timestamp = db.Column(db.DateTime, default=datetime.datetime.utcnow)
     is_anonymous = db.Column(db.Boolean, default=False)
 
-    sender = db.relationship('User', backref='sent_letters', foreign_keys=[sender_id]) 
+    sender = db.relationship('User', backref='sent_letters', foreign_keys=[sender_id])
 
     def __repr__(self):
         return f"<Letter from {self.sender_name} to {self.receiver_email}>"
 
 class LoveHistory(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id')) 
-    name1 = db.Column(db.String(64), nullable=False)
-    name2 = db.Column(db.String(64), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    name1 = db.Column(db.String(60), nullable=False)
+    name2 = db.Column(db.String(60), nullable=False)
     score = db.Column(db.Integer, nullable=False)
-    msg = db.Column(db.Text, nullable=False)
-    date = db.Column(db.DateTime, default=lambda: datetime.datetime.now(datetime.timezone.utc))
+    message = db.Column(db.String(500), nullable=False) 
+    timestamp = db.Column(db.DateTime, default=datetime.datetime.utcnow)
 
     def __repr__(self):
-        return f"<LoveHistory {self.name1} ❤️ {self.name2} = {self.score}%>"
+        return f"<LoveHistory {self.name1} & {self.name2} ({self.score}%)>"
